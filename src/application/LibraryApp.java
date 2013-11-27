1c225587083e331929b5955b496f119d00fa8e12
walkerjonas <j1walker@hsr.ch>
2013-11-27 13:16:19 +0100

    a

--- src/application/LibraryApp.java
+++ src/application/LibraryApp.java
@@ -1,3 +1,10 @@
+//LoanDetailView —> Wenn neue Ausleihe direkt Datum anzeigen lassen wenn man das Buch zurückgeben muss
+//
+//- Maximal 3 Bücher gleichzeitig ausleihen
+//- Nur ausleihe von Kunde A möglich wenn keine Kunde A keine überfälligen  Ausleihen hat
+//- Buchrückgabe
+//- Mahngebühr bei überfälligen ausleihen
+
 package application;
 
 import java.awt.EventQueue;
@@ -15,107 +22,102 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import controller.BookDetailController;
+import view.BookMaster;
+import view.LoanMaster;
 import controller.BookMasterController;
-import controller.LoanDetailController;
 import controller.LoanMasterController;
 import controller.TabbedController;
-import view.BookDetail;
-import view.BookMaster;
-import view.LoanDetailView;
-import view.LoanMaster;
+import domain.Book;
 import domain.Copy;
 import domain.Customer;
 import domain.IllegalLoanOperationException;
 import domain.Library;
 import domain.Loan;
-import domain.Book;
 import domain.Shelf;
+
 //test
 public class LibraryApp {
 	public static void main(String[] args) throws Exception {
 		final Library library = new Library();
 		initLibrary(library);
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
 
-						BookMasterController bookMasterController = new BookMasterController(library, new BookMaster());
-						LoanMasterController loanMasterController = new LoanMasterController(library, new LoanMaster());
-						TabbedController tcontroll = new TabbedController(bookMasterController, loanMasterController);
-//						BookDetailController bookDetailController = new BookDetailController(library, new BookDetail());
-//						BookDetail detail = new BookDetail(library);
-//						detail.frame.setVisible(true);
-//						window.frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+					BookMasterController bookMasterController = new BookMasterController(library, new BookMaster());
+					LoanMasterController loanMasterController = new LoanMasterController(library, new LoanMaster());
+					TabbedController tcontroll = new TabbedController(bookMasterController, loanMasterController);
+					// BookDetailController bookDetailController = new
+					// BookDetailController(library, new BookDetail());
+					// BookDetail detail = new BookDetail(library);
+					// detail.frame.setVisible(true);
+					// window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
+			}
+		});
 	}
 
-	private static void initLibrary(Library library)
-			throws ParserConfigurationException, SAXException, IOException,
-			IllegalLoanOperationException {
-		
+	private static void initLibrary(Library library) throws ParserConfigurationException, SAXException, IOException, IllegalLoanOperationException {
+
 		DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 
-		loadCustomersFromXml(library, builder,new File("data/customers.xml"));
-		
-		loadBooksFromXml(library, builder,new File("data/books.xml"));
-		
+		loadCustomersFromXml(library, builder, new File("data/customers.xml"));
+
+		loadBooksFromXml(library, builder, new File("data/books.xml"));
+
 		// create pseudo random books and loans
 		createBooksAndLoans(library);
-	
+
 		System.out.println("Initialisation of the library was successful!\n");
 		System.out.println("Books in library: " + library.getBooks().size());
 		System.out.println("Customers: " + library.getCustomers().size() + "\n");
 		System.out.println("Copies in library: " + library.getCopies().size());
 		System.out.println("Copies currently on loan: " + library.getLentOutBooks().size());
-		int lentBooksPercentage = (int)(((double)library.getLentOutBooks().size()) / library.getCopies().size() * 100);
+		int lentBooksPercentage = (int) (((double) library.getLentOutBooks().size()) / library.getCopies().size() * 100);
 		System.out.println("Percent copies on loan: " + lentBooksPercentage + "%");
 		System.out.println("Copies currently overdue: " + library.getOverdueLoans().size());
-		
-		for(Loan l : library.getOverdueLoans())
+
+		for (Loan l : library.getOverdueLoans())
 			System.out.println(l.getDaysOverdue());
 	}
 
-	private static void createBooksAndLoans(Library library)
-			throws IllegalLoanOperationException {
-		for(int i = 0; i < library.getBooks().size(); i++) {
-			switch(i%4) {
+	private static void createBooksAndLoans(Library library) throws IllegalLoanOperationException {
+		for (int i = 0; i < library.getBooks().size(); i++) {
+			switch (i % 4) {
 			case 0:
 				Copy c1 = library.createAndAddCopy(library.getBooks().get(i));
 				c1.setCondition(Copy.Condition.GOOD);
-				createLoansForCopy(library,c1,i,5);
+				createLoansForCopy(library, c1, i, 5);
 				Copy c2 = library.createAndAddCopy(library.getBooks().get(i));
 				c2.setCondition(Copy.Condition.DAMAGED);
-				createLoansForCopy(library,c2,i,2);
+				createLoansForCopy(library, c2, i, 2);
 				Copy c3 = library.createAndAddCopy(library.getBooks().get(i));
 				c3.setCondition(Copy.Condition.WASTE);
 				break;
 			case 1:
 				Copy c4 = library.createAndAddCopy(library.getBooks().get(i));
-				createLoansForCopy(library,c4,i,4);
+				createLoansForCopy(library, c4, i, 4);
 				library.createAndAddCopy(library.getBooks().get(i));
 				break;
 			case 2:
 				Copy c5 = library.createAndAddCopy(library.getBooks().get(i));
-				createLoansForCopy(library,c5,i,2);
+				createLoansForCopy(library, c5, i, 2);
 				break;
 			case 3:
 				Copy c6 = library.createAndAddCopy(library.getBooks().get(i));
-				createOverdueLoanForCopy(library,c6,i);
+				createOverdueLoanForCopy(library, c6, i);
 				break;
 			}
 		}
 	}
 
-	private static void loadBooksFromXml(Library library,
-			DocumentBuilder builder, File file) throws SAXException, IOException {
+	private static void loadBooksFromXml(Library library, DocumentBuilder builder, File file) throws SAXException, IOException {
 		Document doc2 = builder.parse(file);
 		NodeList titles = doc2.getElementsByTagName("title");
-		for(int i = 0; i < titles.getLength(); i++) {
+		for (int i = 0; i < titles.getLength(); i++) {
 			Node title = titles.item(i);
 			Book b = library.createAndAddBook(getTextContentOf(title, "name"));
 			b.setAuthor(getTextContentOf(title, "author"));
@@ -124,47 +126,44 @@ public class LibraryApp {
 		}
 	}
 
-	private static void loadCustomersFromXml(Library library,
-			DocumentBuilder builder, File file) throws SAXException, IOException {
+	private static void loadCustomersFromXml(Library library, DocumentBuilder builder, File file) throws SAXException, IOException {
 		Document doc = builder.parse(file);
 		NodeList customers = doc.getElementsByTagName("customer");
-		for(int i = 0; i < customers.getLength(); i++) {
+		for (int i = 0; i < customers.getLength(); i++) {
 			Node customer = customers.item(i);
-			
-			Customer c = library.createAndAddCustomer(getTextContentOf(customer,"name"),getTextContentOf(customer,"surname"));
-			c.setAdress(getTextContentOf(customer,"street"), Integer.parseInt(getTextContentOf(customer,"zip")), getTextContentOf(customer,"city"));
+
+			Customer c = library.createAndAddCustomer(getTextContentOf(customer, "name"), getTextContentOf(customer, "surname"));
+			c.setAdress(getTextContentOf(customer, "street"), Integer.parseInt(getTextContentOf(customer, "zip")), getTextContentOf(customer, "city"));
 		}
 		Collections.sort(library.getCustomers());
 	}
 
-	private static void createLoansForCopy(Library library, Copy copy, int position,
-			int count) throws IllegalLoanOperationException {
+	private static void createLoansForCopy(Library library, Copy copy, int position, int count) throws IllegalLoanOperationException {
 		// Create Loans in the past
-		for(int i = count; i > 1; i--) {
-			Loan l = library.createAndAddLoan(getCustomer(library,position + i), copy);
+		for (int i = count; i > 1; i--) {
+			Loan l = library.createAndAddLoan(getCustomer(library, position + i), copy);
 			GregorianCalendar pickup = l.getPickupDate();
 			pickup.add(GregorianCalendar.MONTH, -i);
-			pickup.add(GregorianCalendar.DAY_OF_MONTH, position%10);
+			pickup.add(GregorianCalendar.DAY_OF_MONTH, position % 10);
 			l.setPickupDate(pickup);
 			GregorianCalendar ret = (GregorianCalendar) pickup.clone();
-			ret.add(GregorianCalendar.DAY_OF_YEAR, position%10+i*2);
+			ret.add(GregorianCalendar.DAY_OF_YEAR, position % 10 + i * 2);
 			l.returnCopy(ret);
 		}
 		// Create actual open loans
-		if(position%2 == 0) {
-			Loan l = library.createAndAddLoan(getCustomer(library,position), copy);
+		if (position % 2 == 0) {
+			Loan l = library.createAndAddLoan(getCustomer(library, position), copy);
 			GregorianCalendar pickup = l.getPickupDate();
-			pickup.add(GregorianCalendar.DAY_OF_MONTH, -position%10);
+			pickup.add(GregorianCalendar.DAY_OF_MONTH, -position % 10);
 			l.setPickupDate(pickup);
 		}
 	}
-	
-	private static void createOverdueLoanForCopy(Library library, Copy copy, int position)
-		throws IllegalLoanOperationException {
+
+	private static void createOverdueLoanForCopy(Library library, Copy copy, int position) throws IllegalLoanOperationException {
 		Loan l = library.createAndAddLoan(getCustomer(library, position), copy);
 		GregorianCalendar pickup = l.getPickupDate();
 		pickup.add(GregorianCalendar.MONTH, -1);
-		pickup.add(GregorianCalendar.DAY_OF_MONTH, -position%15);
+		pickup.add(GregorianCalendar.DAY_OF_MONTH, -position % 15);
 		l.setPickupDate(pickup);
 	}
 
@@ -173,9 +172,9 @@ public class LibraryApp {
 	}
 
 	private static String getTextContentOf(Node element, String name) {
-		NodeList attributes = element.getChildNodes();		
-		for(int r = 0; r < attributes.getLength(); r++) {
-			if(attributes.item(r).getNodeName().equals(name)) {
+		NodeList attributes = element.getChildNodes();
+		for (int r = 0; r < attributes.getLength(); r++) {
+			if (attributes.item(r).getNodeName().equals(name)) {
 				return attributes.item(r).getTextContent();
 			}
 		}
